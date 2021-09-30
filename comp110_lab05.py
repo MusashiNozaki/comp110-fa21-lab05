c8629dab6b555e8174679ec9c87499596bb86a2b
mnozaki <mnozaki@sandiego.edu>
2021-09-30 10:24:49 -0700

    lab5

--- comp110_lab05.py
+++ comp110_lab05.py
@@ -15,7 +15,15 @@ def max_wind_speed(hurricane_filename):
     (type: int) The maximum wind speed of the hurricane.
     """
 
-    pass # replace this line with your implementation of this function
+    f = open(hurricane_filename, 'r')
+    biggest = 0
+    for line in f:
+        vals = line.split(',')
+        wind = int(vals[-2])
+        if wind > biggest:
+            biggest = wind
+    return biggest
+    #print(biggest)
 
 
 def contains_word(word, review):
@@ -34,20 +42,40 @@ def contains_word(word, review):
     otherwise.
     """
 
-    pass # replace this line with your implementation of this function
+    low_word = word.lower()
+    low_review = review.lower()
 
+    split_review = low_review.split(' ')
 
+    return low_word in split_review
+        
 def test_max_wind_speed():
     """ Function that tests the max_wind_speed function. """
-
     print("Starting test of max_wind_speed")
+    result_irma = max_wind_speed('irma.csv')
+    if result_irma == 185:
+        print('PASSED')
+    else:
+        print('FAILED')
 
+    result_florence = max_wind_speed('florence.csv')
+    if result_florence == 140:
+        print('PASSED')
+    else:
+        print('FAILED')
+
+    result_dorian = max_wind_speed('dorian.csv')
+    if result_dorian == 185:
+        print('PASSED')
+    else:
+        print('FAILED')
+    
     # To Do: Call your max_wind_speed function on the irma.csv file, using an if
     # statement to indicate whether the result was correct or not.
     # Then repeat the process for the florence.csv and dorian.csv files to check
     # whether your function works for those files.
 
-    print("FAILED: Not implemented yet.") # remove this line when you finish the to do
+    #print("FAILED: Not implemented yet.") # remove this line when you finish the to do
 
     print("Done testing max_wind_speed")
 
@@ -57,11 +85,11 @@ def test_contains_word():
 
     print("\nStarting test of contains word")
 
-    if contains_word('ok', 'ok') != True:
+    if contains_word('Good', 'the movie was good') != True:
         print("FAILED: contains_word('ok', 'ok')")
-    elif contains_word('ok', 'OK') != True:
+    elif contains_word('horrible', 'The movie was Horrible') != True:
         print("FAILED: contains_word('ok', 'OK')")
-    elif contains_word('ok', 'bad') != False:
+    elif contains_word('no', 'knowledgeble') != False:
         print("FAILED: contains_word('ok', 'bad')")
     elif contains_word('ok', 'movie ok') != True:
         print("FAILED: contains_word('ok', 'movie ok')")
